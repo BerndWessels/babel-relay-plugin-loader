c6344865053f1a4051c12291bd46030b4950a1e4
BerndWessels <wesselsbernd@gmail.com>
2015-10-19 15:26:30 +1300

    Allow babel-node with .babelrc to succeed even if schema.json does not exist.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+var fs = require('fs');
 var path = require('path');
 var getbabelRelayPlugin = require('babel-relay-plugin');
 var config = require('../../package.json');
 
-var schemaPath = path.join('../../', config.metadata.graphql.schema);
-var schema = require(schemaPath);
+try {
+    var schemaPath = path.join(__dirname, '../../', config.metadata.graphql.schema);
 
-module.exports = getbabelRelayPlugin(schema.data);
+    var stats = fs.lstatSync(schemaPath);
+
+    if (stats.isFile()) {
+        var schema = require(schemaPath);
+    }
+}
+catch (e) {
+    console.log(e);
+}
+
+if (schema) {
+    module.exports = getbabelRelayPlugin(schema.data);
+} else {
+    module.exports = getbabelRelayPlugin();
+}
